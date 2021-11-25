74aec5bb2112e85a5ee1cae45eb745393ba3a9fe
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-25 17:37:06 -0500

    add context

---
+++ apollo/urls.js
@@ -0,0 +1,2 @@
+export const URL_BASE =
+    'https://618bf0767657d30008442369--agitated-mccarthy-4ae245.netlify.app/api/'
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { GlobalStyle } from '../public/styles/GlobalStyle'
+// import { LayoutC as Layout } from '../container/Layout'
+import Context from '../context'
+
+import '../public/styles/App.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Context>
+      <GlobalStyle />
+      {/* <Layout> */}
+        <Component {...pageProps} />
+      {/* </Layout> */}
+    </Context>
+  )
 }
 
 export default MyApp

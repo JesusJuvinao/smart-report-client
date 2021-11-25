049fa050f2f6fcdc9a64d16d69a273ff4b47c981
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-25 16:20:06 -0500

    add config env next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const { ADMIN_URL } = process.env
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
+
 module.exports = {
   future: {
     webpack5: true // by default, if you customize webpack config, they switch back to version 4. 
@@ -5,6 +9,29 @@ module.exports = {
   },
   webpack(config) {
     config.resolve.fallback = { fs: false }
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
     return config
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/admin',
+        destination: `${ADMIN_URL}/admin`,
+      },
+      {
+        source: '/admin/:path*',
+        destination: `${ADMIN_URL}/admin/:path*`,
+      },
+    ]
+  },
+}
+
+module.exports = {
+  env: {
+    customKey: 'my-value'
   }
 }

328988027c08f183bb866f709998cdbb096b4948
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-25 15:31:57 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const { ADMIN_URL } = process.env
+
 module.exports = {
-  reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: '/:path*',
+            },
+            {
+                source: '/admin',
+                destination: `${ ADMIN_URL }/admin`,
+            },
+            {
+                source: '/admin/:path*',
+                destination: `${ ADMIN_URL }/admin/:path*`,
+            },
+        ]
+    },
 }

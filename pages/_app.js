d82dc0b7af1362d5d48f5dc413b13791b64e2936
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-25 18:18:36 -0500

    configurando apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,27 @@
 import '../styles/globals.css'
 import { GlobalStyle } from '../public/styles/GlobalStyle'
-import useApollo from '../apollo/apolloClient'
+import { useApollo } from '../apollo/apolloClient'
 // import { LayoutC as Layout } from '../container/Layout'
 import Context from '../context'
 import '../public/styles/App.css'
+import { ApolloProvider } from '@apollo/client'
 // eslint-disable-next-line
 console.log('%c WARNING :', 'color:red;font-size:28px;');
 // eslint-disable-next-line
 console.log('%c CONSOLE ONLY FOR WEB DEVELOPERS', 'color:red;font-size:12px;');
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps)
+
   return (
     <Context>
-      <GlobalStyle />
-      {/* <Layout> */}
+      <ApolloProvider client={apolloClient}>
+        <GlobalStyle />
+        {/* <Layout> */}
         <Component {...pageProps} />
-      {/* </Layout> */}
+        {/* </Layout> */}
+      </ApolloProvider>
     </Context>
   )
 }
 // export default useApollo(MyApp)
 
-export default useApollo(MyApp)

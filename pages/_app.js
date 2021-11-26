b9ad766d4fd0dc0181931dfc79c724b0ba7ddd5b
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-26 07:19:21 -0500

    add ctx

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 import { GlobalStyle } from '../public/styles/GlobalStyle'
 import { useApollo } from '../apollo/apolloClient'
+import Context from '../context'
 import '../public/styles/App.css'
 import { ApolloProvider } from '@apollo/client'
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps)
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <Context>
+      <ApolloProvider client={apolloClient}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </Context>
   )
 }

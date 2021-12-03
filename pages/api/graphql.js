1e7896982a22c05fe4ea292db20a78537f53b6f9
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-12-03 16:28:00 -0500

    add push

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -4,17 +4,18 @@ import resolvers from '../api/lib/resolvers/index'
 import { ApolloServer } from 'apollo-server-micro'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 connectDb()
+console.log('graphql')
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
+  introspection: true,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 const startServer = apolloServer.start();
 
 export default async function handler(req, res) {
-
   await startServer;
   await apolloServer.createHandler({
     path: "/api/graphql",

e376b9afbabd827084af2ef4019aa612674bd34b
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-12-03 16:39:34 -0500

    add push

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,29 +1,32 @@
 import connectDb from './lib/db'
 import typeDefs from '../api/lib/typeDefs'
 import resolvers from '../api/lib/resolvers/index'
-import { ApolloServer } from 'apollo-server-micro'
+import Cors from 'micro-cors'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServer } from 'apollo-server-micro'
 connectDb()
-console.log('graphql')
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  introspection: true,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
-
-const startServer = apolloServer.start();
-
-export default async function handler(req, res) {
-  await startServer;
-  await apolloServer.createHandler({
-    path: "/api/graphql",
-  })(req, res);
-}
-
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+const cors = Cors();
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const startServer = server.start();
+
+export default cors(async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await server.createHandler({ path: "/api/graphql" })(req, res);
+});

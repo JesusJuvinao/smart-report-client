9c430ad38b7957788f1e88b45d029ba75a21b9e1
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-12-03 16:22:49 -0500

    push code playground

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,21 +1,28 @@
 import connectDb from './lib/db'
-const { ApolloServer, gql } = require('apollo-server-micro');
-
-const typeDefs = gql`
-  type Query {
-    sayHello: String
-  }
-`;
+import typeDefs from '../api/lib/typeDefs'
+import resolvers from '../api/lib/resolvers/index'
+import { ApolloServer } from 'apollo-server-micro'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 connectDb()
-const resolvers = {
-  Query: {
-    sayHello(parent, args, context) {
-      return 'Hello World!';
-    },
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
   },
 };
-
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-module.exports = apolloServer.start().then(() => {
-  return apolloServer.createHandler({ path: '/api/graphql' });
-});

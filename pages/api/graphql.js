9f5ff3646d913952ba8105d2103a23a852901609
jesusjuvinao <jesus.juvinao@smartaccountingonline.com>
2021-12-11 09:38:33 -0500

    run helllo

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,5 @@
 import connectDb from './lib/db'
-import typeDefs from '../api/lib/typeDefs'
+// import typeDefs from '../api/lib/typeDefs'
 // import resolvers from '../api/lib/resolvers/index'
 import Cors from 'micro-cors'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
@@ -13,11 +13,11 @@ export const config = {
 
 const cors = Cors();
 
-// const typeDefs = gql`
-//   type Query {
-//     sayHello: String
-//   }
-// `;
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
 
 const resolvers = {
   Query: {
